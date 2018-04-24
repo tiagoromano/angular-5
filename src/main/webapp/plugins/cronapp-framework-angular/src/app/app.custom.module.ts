1bdd595047c86aede6519284f2ecb40a7f96af87
Adir Lima <adir.santos@techne.com.br>
2018-04-24 10:21:11 -0300

    correção merge: app.custom.module.ts.

---
+++ src/main/webapp/plugins/cronapp-framework-angular/src/app/app.custom.module.ts
@@ -0,0 +1,83 @@
+// import { NewComponentComponent } from './new-component/new-component.component';
+import { NgModule } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HelperServiceProvider } from '../providers/helper-service/helper-service';
+import { CommonVariableProvider } from '../providers/common-variable/common-variable';
+import { DatasourceManagerProvider } from '../providers/datasource-manager/datasource-manager';
+import { ValidDirective } from '../directives/valid/valid.directive';
+import { CronValidDirective } from '../directives/valid/cron-valid.directive';
+import { DatasourceDirective } from '../directives/datasource/datasource.directive';
+import { MaskDateDirective } from '../directives/mask/mask-date.directive';
+import { CronMaskDateDirective } from '../directives/mask/cron-mask-date.directive';
+import { CronappSecurityDirective } from '../directives/cronapp-security/cronapp-security.directive';
+import { CronSecurityDirective } from '../directives/cronapp-security/cron-security.directive';
+import { UiSelectDirective } from '../directives/select/ui-select.directive';
+
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { ImportClass } from './common/importClass.component';
+
+
+// import {Component, VERSION, ComponentFactoryResolver, InjectionToken,Injector, ElementRef, Output, Input, EventEmitter,
+//         ComponentFactory, ComponentRef} from '@angular/core';
+// import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { SimpleNotificationsModule } from 'angular2-notifications';
+
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, '../../../i18n/locale_', '.json');
+}
+
+@NgModule({
+    declarations: [
+        DatasourceDirective,
+        CronappSecurityDirective,
+        CronSecurityDirective,
+        ValidDirective,
+        CronValidDirective,
+        MaskDateDirective,
+        CronMaskDateDirective,
+        UiSelectDirective
+    ],
+    imports: [
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient]
+            }
+        }),
+        FormsModule,
+        BrowserModule,
+        BrowserAnimationsModule
+    ],
+    exports: [
+        TranslateModule,
+        FormsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        DatasourceDirective,
+        CronappSecurityDirective,
+        CronSecurityDirective,
+        ValidDirective,
+        CronValidDirective,
+        MaskDateDirective,
+        CronMaskDateDirective,
+        UiSelectDirective
+    ],
+    providers: [
+
+        // HelperServiceProvider,
+        // CommonVariableProvider,
+        // DatasourceManagerProvider
+    ],
+    bootstrap: [],
+})
+
+export class AppCustomModule {
+
+}
+
